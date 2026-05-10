a109204a9c4683d05e362bd53acdd4dbdea53ac8
dessertrando <2thhqkkmrp@privaterelay.appleid.com>
2026-05-09 21:32:54 -0500

    firebase.js

--- firebase.js
+++ firebase.js
@@ -1,11 +1,19 @@
+
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 
 import {
-  getAuth
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut,
+  onAuthStateChanged
 } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 
 import {
-  getFirestore
+  getFirestore,
+  doc,
+  setDoc,
+  getDoc
 } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
 const firebaseConfig = {
@@ -15,8 +23,22 @@ const firebaseConfig = {
   messagingSenderId: "586754316774",
   appId: "1:586754316774:web:4ad2cda8e218254b91de27"
 };
+};
 
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
 export const db = getFirestore(app);
+
+export {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut,
+  onAuthStateChanged,
+  doc,
+  setDoc,
+  getDoc
+};
+
+
+
