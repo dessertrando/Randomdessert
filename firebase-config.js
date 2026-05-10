779b019e3873a31a77c3145972f688d77bb96597
dessertrando <2thhqkkmrp@privaterelay.appleid.com>
2026-05-09 23:18:34 -0500

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
-  apiKey: "PASTE_API_KEY",
-  authDomain: "PASTE_PROJECT.firebaseapp.com",
-  projectId: "PASTE_PROJECT_ID",
-  storageBucket: "PASTE_BUCKET.appspot.com",
-  messagingSenderId: "PASTE_SENDER_ID",
-  appId: "PASTE_APP_ID"
+  apiKey: "AIzaSyC-SNHdJr0jN3Ecr15EPzmdbZlRLFtWL7A",
+  authDomain: "dessert-explorer.firebaseapp.com",
+  projectId: "dessert-explorer",
+  storageBucket: "dessert-explorer.firebasestorage.app",
+  messagingSenderId: "586754316774",
+  appId: "1:586754316774:web:4ad2cda8e218254b91de27"
 };
